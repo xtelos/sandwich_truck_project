7dc46a75a52965e28ba7d487ea76d3262a0d9df6
dylanfodor <fodord@moravian.edu>
2022-02-17 09:23:28 -0500

    added basic jframe

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        Truck_Map map = new Truck_Map();
     }
 }
--- src/Truck_Map.java
+++ src/Truck_Map.java
@@ -2,5 +2,11 @@ import javax.swing.*;
 
 public class Truck_Map extends JFrame
 {
+    public Truck_Map() {
+        JFrame frame = new JFrame("Truck Map");
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
 }
