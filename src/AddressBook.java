3e3048d78307e3e38584682680f3abdfb965ebfc
Jacob Smith <smithj09@moravian.edu>
2022-02-17 10:22:27 -0500

    Added Implementation for Address class

---
+++ src/AddressBook.java
@@ -0,0 +1,26 @@
+/*
+This class generates 100 random Address objects and saves their data to a batch file
+
+Author: Jacob Smith
+ */
+
+import java.util.ArrayList;
+
+public class AddressBook
+{
+    private ArrayList<Address> addressBook;
+
+    public AddressBook() {
+        addressBook = new ArrayList<>(100);
+
+    }
+
+    public void generateAddresses() {
+
+    }
+
+    private void writeAddressToFile(Address address) {
+
+    }
+
+}
