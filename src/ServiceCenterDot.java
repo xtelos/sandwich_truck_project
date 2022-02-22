26361551e23252b761d31b85c90eacbca5e3db19
Devin <mccormackd@moravian.edu>
2022-02-22 09:54:55 -0500

    added documentation to ServiceCenterDot and removed Destination from ServiceCenterDot

--- src/ServiceCenterDot.java
+++ src/ServiceCenterDot.java
@@ -3,25 +3,35 @@ import java.awt.*;
 public class ServiceCenterDot extends Representations{
 
     private Location location;
-    private Location destination;
 
-    ServiceCenterDot(Location location, Location destination) {
-        super(location, destination);
+    /**
+     * Constructor with a specific start location within parameter
+     */
+    ServiceCenterDot(Location location) {
+        super(location);
         this.location = location;
-        this.destination = destination;
 
     }
 
+    /**
+     * Constructor without a specific start location
+     */
     ServiceCenterDot() {
-        Location location = new Location();
+        Location location = new Location(500,500);
         this.location = location;
 
     }
 
+    /**
+     * Empty update(), as the service center will not need to be moved during operation
+     */
     @Override
     public void update() {
     }
 
+    /**
+     * Places the service center on the map as a blue dot
+     */
     @Override
     public void paint(Graphics g) {
         Color color = g.getColor();
