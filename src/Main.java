71f25f2e1ce19a389e8f5e17d011e95bc760209d
Jacob Smith <smithj09@moravian.edu>
2022-03-16 19:40:20 -0400

    Started to implement reading batchfile with orders

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
@@ -5,7 +11,17 @@ public class Main
         TruckSim sim = new TruckSim(1000,1000);
         sim.runSim();
 
-        OrderGenerator orderGenerator = new OrderGenerator();
-        orderGenerator.generate(100);
+        HashSet<Order> orders = new HashSet<>();
+        try {
+            File file = new File("orders.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(",");
+            }
+        } catch (FileNotFoundException exception) {
+            System.out.println("Couldn't read file");
+        }
     }
 }
