0eefb1fa95a3b0d5778c7ef42160441ec74ac371
Jacob Smith <smithj09@moravian.edu>
2022-03-31 10:29:32 -0400

    Added documentation for Sprint3

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,10 +1,30 @@
+/*
+Abstract class that represents a Sandwich object for orders to the truck
+
+Author: Jacob Smith
+ */
+
 public abstract class Sandwich {
 
     String description = "Unknown Sandwich";
 
+    /**
+     * Returns the description of the Sandwich to the caller
+     *
+     * @return: String
+     *
+     * Author: Jacob Smith
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the cost of the Sandwich to the caller
+     * 
+     * @return: double
+     *
+     * Author: Jacob Smith
+     */
     public abstract double cost();
 }
--- src/SandwichDecorator.java
+++ src/SandwichDecorator.java
@@ -1,3 +1,8 @@
+/*
+Provides a class to add toppings to a Sandwich which implements the Decorator Design Pattern for Sandwich orders
+
+Author: Jacob Smith
+ */
 public abstract class SandwichDecorator extends Sandwich {
     public abstract String getDescription();
 }
